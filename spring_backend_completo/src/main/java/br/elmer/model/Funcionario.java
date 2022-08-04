0211567ee3a4a01440350f8e4db3432ebd768706
Elmer Farias <57157002+elmercfarias@users.noreply.github.com>
2022-08-04 20:49:40 -0300

    Add files via upload

---
+++ spring_backend_completo/src/main/java/br/elmer/model/Funcionario.java
@@ -0,0 +1,68 @@
+package br.elmer.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Funcionario {
+
+	private long id;
+	private String nome;
+	private String sobrenome;
+	private String email;
+	
+	public Funcionario() {
+		
+	}
+	
+	public Funcionario(String nome, String sobrenome, String email) {
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.email = email;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	@Column(name = "first_name", nullable = false)
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	@Column(name = "last_name", nullable = false)
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	@Column(name = "email_address", nullable = false)
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Funcionariov2 [v2id=" + id + ", nome=" + nome + ", sobrenome=" + sobrenome + ", email=" + email
+				+ "]";
+	}
+	
+}
